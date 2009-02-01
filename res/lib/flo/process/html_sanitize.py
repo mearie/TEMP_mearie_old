cd75f1d74cc14967088e5311f4a62483db73cddf
Kang Seonghoon <public+hg@mearie.org>
2009-02-01 17:56:51 +0900

    added transform feature to sanitizer. (TODO: non-verbose interface)

--- res/lib/flo/process/html_sanitize.py
+++ res/lib/flo/process/html_sanitize.py
@@ -3,6 +3,8 @@
 
 from __future__ import absolute_import, division, with_statement
 
+from xml.dom.minidom import parse, parseString
+
 class HTMLSanitizer(object):
     input_type = ['text/html', 'application/xhtml+xml']
     output_type = 'text/html'
@@ -12,7 +14,18 @@ class HTMLSanitizer(object):
 
     def __call__(self, context, data):
         if data is None:
-            data = open(os.path.join(self.base, context.path), 'r')
+            path = os.path.join(self.base, context.path)
+            xml = parse(open(path, 'r'))
+        else:
+            xml = parseString(data)
 
-        return data.strip()
+        try:
+            for el in xml.getElementsByTagName('foo'):
+                newel = xml.createElement('p')
+                newel.appendChild(xml.createTextNode('FOO!'))
+                el.parentNode.replaceChild(newel, el)
+
+            return xml.toxml(encoding='utf-8')
+        finally:
+            xml.unlink()
 
