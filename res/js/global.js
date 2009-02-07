7a6ce5bcdc4466d728291b6b0035b278c95c60c4
Kang Seonghoon <public+hg@mearie.org>
2009-02-07 21:38:47 +0900

    added MathReplacer and <m> element, global javascript base(/res/js/global.js). /about/contact and /about/kang-seonghoon page is updated.

---
+++ res/js/global.js
@@ -0,0 +1,17 @@
+// mearie.org 3.0 global script base
+// Copyright (c) 2005-2009, Kang Seonghoon.
+// See http://mearie.org/copyright for license information.
+
+$(function() {
+	$('span.math').each(function() {
+		var size = 1; // by default
+		var m = this.className.match(/\bmath-size([0-4]|-[1-4])\b/);
+		if (m) size = m[1];
+
+		var code = $(this).text().replace('&', '&amp;');
+		$(this).replaceWith('<img src="http://l.wordpress.com/latex.php?s=' +
+			size + '&latex=' + code + '" alt="' + code + '" class="' +
+			this.className + '"/>')
+	});
+});
+
