871b843ad8ad9ea0f48907677d6b08fe3626d3d5
Kang Seonghoon <public+hg@mearie.org>
2009-03-15 02:40:12 +0900

    added cached hierarchical configuration (via flo.conf.ConfigCache); added support for .flo/types.conf

--- res/lib/flo/conf.py
+++ res/lib/flo/conf.py
@@ -18,30 +18,25 @@ All configurations, datas such as SQLite database, caches etc. is saved in
 from __future__ import absolute_import, division, with_statement
 
 import os, os.path
+import copy
 import mimetypes
 import ConfigParser as configparser
 
 class Config(object):
-    def __init__(self, base, dir):
-        # append a slash if there is no trailing slash.
-        self.base = os.path.normpath(base).rstrip(os.sep) + os.sep
+    def __init__(self, parent, dir):
+        self.parent = parent
         self.dir = os.path.normpath(dir).rstrip(os.sep) + os.sep
-        assert self.dir[:len(self.base)] == self.base
-        self.dirdepth = self.dir[len(self.base):].count(os.sep)
+        assert self.parent is None or self.dir.startswith(self.parent.dir)
 
         self.localconf = None
+        self.localtypes = None
         self.encoding = None
-        self.localtypes = mimetypes.MimeTypes()
-
-        # TODO move to separate file
-        self.localtypes.add_type('text/plain', '.py')
 
     def paths(self, *trail):
-        dir = self.dir
-        yield os.path.join(dir, *trail)
-        for i in xrange(self.dirdepth):
-            dir = os.path.join(dir, '..')
-            yield os.path.join(dir, *trail)
+        conf = self
+        while conf is not None:
+            yield os.path.join(self.dir, *trail)
+            conf = conf.parent
 
     def flo_paths(self):
         return self.paths('.flo')
@@ -50,14 +45,32 @@ class Config(object):
         return self.paths('.flo', 'http%03d.html' % status)
 
     def read_localconf(self):
-        self.localconf = configparser.SafeConfigParser()
-        for path in self.paths('.flo', 'local.conf'):
-            self.localconf.read(path)
+        if self.parent is None:
+            self.localconf = configparser.SafeConfigParser()
+        else:
+            if self.parent.localconf is None:
+                self.parent.read_localconf()
+            self.localconf = copy.deepcopy(self.parent.localconf)
+
+        path = os.path.join(self.dir, '.flo', 'local.conf')
+        self.localconf.read(path)
 
         try:
             self.encoding = self.localconf.get('global', 'encoding')
         except:
             self.encoding = 'utf-8'
+
+    def read_localtypes(self):
+        if self.parent is None:
+            self.localtypes = mimetypes.MimeTypes()
+        else:
+            if self.parent.localtypes is None:
+                self.parent.read_localtypes()
+            self.localtypes = copy.deepcopy(self.parent.localtypes)
+
+        path = os.path.join(self.dir, '.flo', 'types.conf')
+        if os.path.exists(path):
+            self.localtypes.read(path)
 
     def __getitem__(self, key):
         if self.localconf is None:
@@ -91,5 +104,25 @@ class Config(object):
             return optdefault
 
     def guess_type(self, filename):
+        if self.localtypes is None:
+            self.read_localtypes()
         return self.localtypes.guess_type(filename)
+
+class ConfigCache(object):
+    def __init__(self, app):
+        self.app = app
+        self.cache = app.get_cache('flodirconf', type='memory', expiretime=30)
+
+    def load(self, path):
+        if path == self.app.base:
+            parentconf = None
+        else:
+            parentconf = self.get(os.path.split(path)[0])
+        return Config(parentconf, path)
+
+    def get(self, path):
+        return self.cache.get(path, createfunc=lambda: self.load(path))
+
+    def forget(self, path):
+        raise NotImplemented # how to?
 
