6478990d0caf85c2364f54be022819381e5ee5f4
Kang Seonghoon <public+hg@mearie.org>
2010-01-03 15:14:16 +0900

    imgframe/math construct in library.tmpl.html; updated and added profile & contact pages.

---
+++ res/global.js
@@ -0,0 +1,17 @@
+// replaces <m> elements in the original text.
+$(function() {
+	$('span.math').each(function() {
+		var size = 1; // by default
+		var m = this.className.match(/\bmath-size([0-4]|-[1-4])\b/);
+		if (m) size = m[1];
+
+		var code = $(this).text();
+		var img = document.createElement('img');
+		img.setAttribute('src', 'http://l.wordpress.com/latex.php?latex=' +
+			encodeURIComponent(code) + '&s=' + size + '&bg=eeeeee&fg=444444');
+		img.setAttribute('alt', code);
+		img.className = this.className;
+		$(this).replaceWith(img);
+	});
+});
+
