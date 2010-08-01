f4ccbc0839112cadd895b3cd0e865863bd939329
Kang Seonghoon <public+hg@mearie.org>
2010-08-02 03:00:54 +0900

    copied posts from arachneng.egloos.com (initial phase); fixed a blocking due to the stdin buffer fill in mearie.markdown.

--- bin/mearie/markdown.py
+++ bin/mearie/markdown.py
@@ -10,12 +10,10 @@ def markdown(text, opts=()):
     if html is None:
         popen = subprocess.Popen(['pandoc'] + list(opts),
                 stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-        popen.stdin.write(text.encode('utf-8'))
-        popen.stdin.close()
-        returncode = popen.wait()
-        if returncode != 0:
+        (out, err) = popen.communicate(input=text.encode('utf-8'))
+        if popen.returncode != 0:
             raise RuntimeError('pandoc failed with code %d' % returncode)
-        html = popen.stdout.read().decode('utf-8')
+        html = out.decode('utf-8')
         html = re.sub(ur'\n\s*>', ur'>', html)
         html = re.sub(ur'(&lt;)?(?<!<pre>)<code\b', lambda m: '<code' if m.group(1) else '<tt', html)
         html = re.sub(ur'</code>(?!</pre>)(&gt;)?', lambda m: '</code>' if m.group(1) else '</tt>', html)
